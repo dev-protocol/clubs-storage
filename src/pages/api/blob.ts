9f7208f2a829343326deb01493d608f13c38beff
Aggre <hiroyuki.aggre@gmail.com>
2025-03-27 15:44:49 +0900

    lint

--- src/pages/api/blob.ts
+++ src/pages/api/blob.ts
@@ -27,7 +27,6 @@ export const POST: APIRoute = async ({ request, url }) => {
 		whenDefined(form.get('file'), (file) => file as File) ??
 		new Error('File is missing.')
 
-
 	const eoa =
 		whenDefined(props, ({ signature, message }) =>
 			recoverAddress(hashMessage(message), signature),
