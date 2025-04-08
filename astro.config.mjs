b3d9b3ef7ed3a3349b9d30e93825b792205f2ca0
Aggre <hiroyuki.aggre@gmail.com>
2025-04-08 11:26:39 +0900

    disable `AstroUserConfig.security.checkOrigin`

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,13 @@ import vercel from '@astrojs/vercel/serverless'
 
 import vue from '@astrojs/vue'
 
- 
 config()
 
 export default defineConfig({
 	output: 'server',
+	security: {
+		checkOrigin: false,
+	},
 	adapter: vercel(),
 	integrations: [vue()],
 })
