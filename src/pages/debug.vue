5b450ea55d62c20ec661f2273467f5bc0f92e346
Aggre <hiroyuki.aggre@gmail.com>
2025-09-30 17:58:08 +0900

    feat(debug): enable multiple file uploads and display uploaded blobs

--- src/pages/debug.vue
+++ src/pages/debug.vue
@@ -2,8 +2,11 @@
 import type { PutBlobResult } from '@vercel/blob'
 import { ref, useTemplateRef } from 'vue'
 
+type Res = PutBlobResult & { storageURL: string }
+
 const input = useTemplateRef<HTMLInputElement>('inputFileRef')
-const blob = ref<PutBlobResult>()
+const multipleFiles = ref<{ file: File; url?: string }[]>()
+const blobs = ref<Res[]>()
 const hls = ref<boolean>()
 
 const onSubmit = async (event: Event) => {
@@ -13,27 +16,44 @@ const onSubmit = async (event: Event) => {
 		throw new Error('No file selected')
 	}
 
-	const file = input.value.files[0]
+	const files = input.value.files
+	multipleFiles.value = Array.from(files).map((file) => ({
+		file,
+	}))
 
-	const formData = new FormData()
-	formData.append('file', file)
+	const responses = await Promise.all(
+		Array.from(files).map((file) => {
+			const formData = new FormData()
+			formData.append('file', file)
+			return fetch(`/api/blob${hls.value ? '?convert=hls' : ''}`, {
+				method: 'POST',
+				body: formData,
+			})
+		}),
+	)
 
-	const response = await fetch(`/api/blob${hls.value ? '?convert=hls' : ''}`, {
-		method: 'POST',
-		body: formData,
-	})
+	const newBlobs = (await Promise.all(
+		responses.map((res) => res.json()),
+	)) as Res[]
+	console.table(newBlobs)
+	blobs.value = newBlobs
+}
 
-	const newBlob = (await response.json()) as PutBlobResult
-	console.table(newBlob)
-	blob.value = newBlob
+async function setClipboard(text: string) {
+	const type = 'text/plain'
+	const clipboardItemData = {
+		[type]: text,
+	}
+	const clipboardItem = new ClipboardItem(clipboardItemData)
+	await navigator.clipboard.write([clipboardItem])
 }
 </script>
 
 <template>
-	<h1>Upload Image</h1>
+	<h1>Upload Images</h1>
 
 	<form @submit="onSubmit">
-		<input name="file" ref="inputFileRef" type="file" required />
+		<input name="file" ref="inputFileRef" type="file" multiple required />
 		<input type="checkbox" id="hls" name="hls" v-model="hls" />
 		<label for="hls">Convert video to HLS</label>
 		<button type="submit">Upload</button>
@@ -42,5 +62,47 @@ const onSubmit = async (event: Event) => {
 	<hr />
 	{{ hls }}
 
-	{{ blob }}
+	<ul>
+		<li v-for="blob in blobs" :key="blob.url">
+			<img :src="blob.storageURL" alt="" />
+			<span>{{ blob.contentType }}</span>
+			<span>{{ blob.url }}</span>
+			<span
+				>{{ blob.storageURL }}
+				<button @click="() => setClipboard(blob.storageURL)" class="border">
+					Copy
+				</button></span
+			>
+		</li>
+	</ul>
 </template>
+
+<style scoped>
+ul {
+	list-style: none;
+	padding: 0;
+	display: grid;
+	gap: 1rem;
+}
+li {
+	display: flex;
+	align-items: center;
+	gap: 1rem;
+	& img {
+		width: 4rem;
+		height: 4rem;
+		object-fit: contain;
+		border: 1px solid #ccc;
+		border-radius: 0.25rem;
+	}
+	& span {
+		font-family: monospace;
+		font-size: small;
+	}
+}
+button {
+	border: 1px solid #ccc;
+	border-radius: 3px;
+	padding: 0.25rem 0.5rem;
+}
+</style>
